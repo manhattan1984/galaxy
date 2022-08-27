6b437e757426e079cd2ad3e14333864b3e10222c
Michael Jackson <mikkimanhattan@gmail.com>
2022-08-27 18:44:37 +0100

    update stock widget

--- pages/stocks.jsx
+++ pages/stocks.jsx
@@ -14,10 +14,10 @@ const Stocks = () => {
       <Box>
         <iframe
           name="ScreenerWidget"
-          src="https://darqube.com/external-embedding/5?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ3aWRnZXQiOnsic2NyZWVuZXJfYXNzZXRfY2xhc3NlcyI6WyJTVE9DS1MiLCJGT1JFWCJdfSwid190eXBlIjoiU2NyZWVuZXJXaWRnZXQiLCJmZV9jZmciOnsiY21vZGUiOjAsImZjbHIiOiJyZ2JhKDI1NSwgMjU1LCAyNTUsIDEpIiwiYmciOiJyZ2JhKDIxLCAyNSwgMzAsIDEpIiwiaCI6NTEwLCJ3Ijo5MDAsImFzeiI6dHJ1ZSwiaGRpY24iOmZhbHNlLCJ3dG1WIjp7InR5cGUiOiJEYXJxdWJlIiwiZW5hYmxlZCI6dHJ1ZX19LCJleHAiOjE2NjgwMjIwMjEsInN1YiI6ImFjY2VzcyJ9.KSm6kO4adX38UJK9Hb8BA8qx7nt-FFcruBzVAxAEOzY"
+          src="https://darqube.com/external-embedding/screener-widget?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ3aWRnZXQiOnsic2NyZWVuZXJfYXNzZXRfY2xhc3NlcyI6WyJTVE9DS1MiLCJDUllQVE8iLCJGT1JFWCJdfSwid190eXBlIjoiU2NyZWVuZXJXaWRnZXQiLCJmZV9jZmciOnsiY21vZGUiOjAsImZjbHIiOiJyZ2JhKDI1NSwgMjU1LCAyNTUsIDEpIiwiYmciOiJyZ2JhKDIxLCAyNSwgMzAsIDEpIiwiaCI6NTEwLCJ3Ijo5MDAsImFzeiI6dHJ1ZSwibG5nIjoiZW4iLCJoZGljbiI6ZmFsc2UsInd0bVYiOnsidHlwZSI6IkRhcnF1YmUiLCJlbmFibGVkIjp0cnVlfX0sImV4cCI6MTY2OTM5ODI0NSwic3ViIjoiYWNjZXNzIn0.d9qZ7vFcqfj2DhVCxp7r4A71EkaIDTiaQuM2NwWKksc"
           id="ScreenerWidget"
           width="100%"
-          height="600px"
+          height="100%"
         ></iframe>
       </Box>
 
