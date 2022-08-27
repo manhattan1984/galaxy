017060094ae7d99df7fbc695a7e91c909004b776
Michael Jackson <mikkimanhattan@gmail.com>
2022-08-27 19:52:38 +0100

    minor

--- pages/stocks.jsx
+++ pages/stocks.jsx
@@ -17,7 +17,7 @@ const Stocks = () => {
           src="https://darqube.com/external-embedding/screener-widget?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ3aWRnZXQiOnsic2NyZWVuZXJfYXNzZXRfY2xhc3NlcyI6WyJTVE9DS1MiLCJDUllQVE8iLCJGT1JFWCJdfSwid190eXBlIjoiU2NyZWVuZXJXaWRnZXQiLCJmZV9jZmciOnsiY21vZGUiOjAsImZjbHIiOiJyZ2JhKDI1NSwgMjU1LCAyNTUsIDEpIiwiYmciOiJyZ2JhKDIxLCAyNSwgMzAsIDEpIiwiaCI6NTEwLCJ3Ijo5MDAsImFzeiI6dHJ1ZSwibG5nIjoiZW4iLCJoZGljbiI6ZmFsc2UsInd0bVYiOnsidHlwZSI6IkRhcnF1YmUiLCJlbmFibGVkIjp0cnVlfX0sImV4cCI6MTY2OTM5ODI0NSwic3ViIjoiYWNjZXNzIn0.d9qZ7vFcqfj2DhVCxp7r4A71EkaIDTiaQuM2NwWKksc"
           id="ScreenerWidget"
           width="100%"
-          height="100%"
+          height="600px"
         ></iframe>
       </Box>
 
