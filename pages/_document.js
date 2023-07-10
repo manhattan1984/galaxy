c597441366ea593138701ff30632f77a2573502d
manhattan1984 <103131213+manhattan1984@users.noreply.github.com>
2023-07-10 15:32:56 +0100

    Add Smartups

--- pages/_document.js
+++ pages/_document.js
@@ -17,10 +17,18 @@ const Document = () => {
       <body>
         <Main />
         <NextScript />
-        <Script
-          src="//code.tidio.co/8dhhwe23q9cxc30dp39iwcmfov7aoypm.js"
-          strategy="lazyOnload"
-        />
+          <Script>
+        {`
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b7b8816e4b744e14d300bbf938109767e53abf91';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+`}
+      </Script>
       </body>
     </Html>
   );
